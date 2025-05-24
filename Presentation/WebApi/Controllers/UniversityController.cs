5393f036fcd3d238e1cecfed27b1749da28c5d8b
kosebaris1 <kosebaris464@gmail.com>
2025-05-24 21:41:36 +0300

    GetAllUniversity endpoint completed

---
+++ Presentation/WebApi/Controllers/UniversityController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Features.MediatR.Universities.Queries;
+using Application.Features.MediatR.Users.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class UniversityController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UniversityController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("GetAllUniversity")]
+        public async Task<IActionResult> GetAllUniversity()
+        {
+            var values = await _mediator.Send(new GetAllUniversityQuery());
+            return Ok(values);
+        }
+    }
+}
