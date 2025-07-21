9cfad9d200f9d92b7dc188c0025f522930e4fd86
kosebaris1 <kosebaris464@gmail.com>
2025-07-21 13:14:10 +0300

    feat(admin):add report listing feature to admin dashboard

---
+++ Frontend/UniWebUI/Areas/Admin/ViewComponents/AdminMainViewComponents/_AdminReportListComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace UniWebUI.Areas.Admin.ViewComponents.AdminMainViewComponents
+{
+    public class _AdminReportListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminReportListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7224/api/Report");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<List<UniDto.ReportDtos.GetReportDto>>(jsonData);
+                return View(result);
+            }
+
+            return View();
+        }
+    }
+}
